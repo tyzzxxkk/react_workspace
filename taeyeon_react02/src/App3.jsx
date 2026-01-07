9564bbf3c38f9f8170c20c97882125bb7ce9be7b
태연 <s2401@e-mirim.hs.kr>
2026-01-07 22:49:47 +0900

    app

--- taeyeon_react02/src/App3.jsx
+++ taeyeon_react02/src/App3.jsx
@@ -1,5 +1,6 @@
-import Tabs from "./components/Tabs";
 import { useState } from "react";
+import Tabs from "./components/Tabs";
+import SearchForm from "./components/SearchForm";
 
 function App() {
 
