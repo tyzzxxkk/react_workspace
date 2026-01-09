1ec820a3c6300875654f6dd11a929f0ca182589a
태연 <s2401@e-mirim.hs.kr>
2026-01-09 09:24:42 +0900

    app

--- taeyeon_react03/src/App.jsx
+++ taeyeon_react03/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import Controller from "./components/Controller";
+import Viewer from "./components/Viewer";
+import Even from "./components/Even";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const isMount = useRef(false);
+
+ useEffect(() => {
+  if(!isMount.current){
+    console.log("마운트");
+    isMount.current = true;
+    return;
+  }
+  console.log("useEffct 업데이트");
+});
+
+  const onClickButton = (num) => {
+    setCount(count + num);
+  };
 
   return (
     <>
+      <h1>Simple Counter</h1>
+      <input
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      {input}
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <Controller onClickButton={onClickButton} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
