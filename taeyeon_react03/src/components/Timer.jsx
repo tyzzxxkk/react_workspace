f9adc25a1314feec970ccd5e7bc2c466447feed3
태연 <s2401@e-mirim.hs.kr>
2026-01-10 16:32:17 +0900

    component

--- taeyeon_react03/src/components/Timer.jsx
+++ taeyeon_react03/src/components/Timer.jsx
@@ -1,16 +1,24 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Timer() {
-  const [seconds, setSeconds] = useState(0);
+    const [seconds, setSeconds] = useState(0);
 
-  useEffect(() => {
-    const intervalVar = setInterval(() => {
-      setSeconds((prev) => prev + 1);
-    }, 1000);
-    return () => {};
-  }, []);
+    useEffect(() => {
+        const timer = setInterval(()=>{
+            setSeconds((prev) => prev+1);
+            return () => {clearInterval(intervalVar);};
+        }, 1000);
 
-  return <h1>타이머 : {seconds}초</h1>;
+        return () => {
+            clearInterval(timer);
+        };
+    },[]);
+
+    return (
+        <>
+        <h1>타이머: {seconds}초</h1>
+        </>
+    );
 }
 
 export default Timer;
--- taeyeon_react03/src/components/Userlist.jsx
+++ taeyeon_react03/src/components/Userlist.jsx
@@ -24,3 +24,5 @@ const Userlist = () => {
         </>
     );
 }
+
+export default Userlist;
